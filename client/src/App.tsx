aa23c99a89827d494bbb15342f0d9e1cc84e3c7d
HADJADJDAOUD <daoudhdj@gmail.com>
2024-12-20 20:21:11 +0100

    added some charts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import { MyChart } from "./components/MyChart";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ModeToggle } from "@/components/mode-toggle";
+import { RadialChart } from "./components/RadialChart";
+import { AreaChart } from "./components/AreaChart";
 export default function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       
       <MyChart />
       <ModeToggle />
+      <RadialChart />
+      <AreaChart />
     </ThemeProvider>
   );
 }
